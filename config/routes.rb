eabbca74b57fb7320d558c7f3bdf0d2460eac531
Alan Quevreux <alan.quevreux@gmail.com>
2024-07-03 16:59:42 +0200

    yelp

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # Pour voir la liste de tous les restaurants
+  get 'restaurants', to: 'restaurants#index'
+
+  # Pour ajouter un nouveau restaurant
+  get 'restaurants/new', to: 'restaurants#new'
+  post 'restaurants', to: 'restaurants#create'
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+
+  # Pour voir les détails d’un restaurant, avec tous les avis associés au restaurant
+  get 'restaurants/:id', to: 'restaurants#show'
+
+  #  Pour ajouter un nouvel avis à un restaurant
+  get 'restaurants/:id/reviews/new', to: 'reviews#new'
+  post 'restaurants/:id/reviews', to: 'reviews#create'
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Supprime tous les restaurants avant de créer de nouveaux
+puts "Cleaning database..."
+Restaurant.destroy_all
+# Crée de nouveaux restaurants
+puts "Creating restaurants..."
+barakafrit = {name: "Barakafrit", address: "169 Rue de Montmartre, Paris", category: "belgian"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian"}
+chez_claude = {name: "Chez Claude", address: "Place de la République, Paris", category: "french"}
+dazuma = {name: "Dazuma", address: "44 Rue Beaurepaire, Angers", category: "japanese"}
+wok_city = {name: "Wok City", address: "10 Avenue de l'Opéra, Paris", category: "chinese"}
+
+[barakafrit, pizza_east, chez_claude, dazuma, wok_city].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
